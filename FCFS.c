2e8d4cafefb9ee6ecdd69051ad100fe2650f88b0
r-best <bobbyisbest3@gmail.com>
2018-03-06 14:20:35 -0500

    Works except for large numbers of  producers

--- FCFS.c
+++ FCFS.c
@@ -18,15 +18,20 @@ sem_t mutex;
 int numProducers, numConsumers, counter;
 
 void initPrintJob(struct PrintJob *job){
-    job->size = rand() % (1000 + 1 - 100) + 100;
+    // printf("Initializing print job\n");
+    job->size = 2;//rand() % (1000 + 1 - 100) + 100;
 }
 
 void* producerFn(void *args){
     printf("Starting producer thread\n");
-    int numJobs = (rand() % 20) + 1;
+    int numJobs = 1;//(rand() % 20) + 1;
     for(int i = 0; i <= numJobs; i++){
         struct PrintJob *job = malloc(sizeof(struct PrintJob));
-        if(job->size != numJobs)
+        if(job == NULL){
+            printf("Error allocating memory for print job\n");
+            exit(-1);
+        }
+        if(i < numJobs)
             initPrintJob(job);
         else // If all jobs done, insert a flag job instead (-1 size to tell consumers you're done)
             job->size = -1;
@@ -37,47 +42,58 @@ void* producerFn(void *args){
         printf("Produced %d\n", job->size);
         sem_post(&mutex);
     }
+    printf("Producer thread exiting\n");
     pthread_exit(0);
 }
 
 void* consumerFn(void *args){
     printf("Starting consumer thread\n");
-    while(1){
+    while(counter < numProducers){
         sem_wait(&empty);
         sem_wait(&mutex);
-        sem_post(&empty);
         struct PrintJob *job = popJob();
-        int size = job->size;
+        // sem_post(&empty);
         sem_post(&mutex);
+        if(job == NULL){
+            // sem_post(&mutex);
+            continue;
+        }
+        int size = job->size;
         free(job);
 
         // If this is a flag job, increment counter
         if(size < 0){
             counter++;
+            printf("COUNTER: %d %d\n", counter, numProducers);
             // If counter has reached numProducers, all producers are done so break
-            if(counter == numProducers)
-                break;
+            if(counter == numProducers){
+                printf("YES\n");
+                // break;
+            }
         }
         printf("Consumed %d\n", size);
-        sleep(size);
+        sleep(size / 1000);
     }
+    printf("Consumer thread exiting\n");
+    sem_post(&empty);
+    printf("A\n");
     pthread_exit(0);
 }
 
 int main(int argc, char *argv[]){
     // Set up signal handler
-    struct sigaction action;
-	action.sa_handler = signalHandler;
-	action.sa_flags = SA_NODEFER | SA_ONSTACK;
-	sigaction(SIGINT, &action, NULL);
+    // struct sigaction action;
+	// action.sa_handler = signalHandler;
+	// action.sa_flags = SA_NODEFER | SA_ONSTACK;
+	// sigaction(SIGINT, &action, NULL);
 
     // Process args
     if(argc < 3){
         printf("Program requires at least 2 arguments\n");
         exit(-1);
     }
-    int numProducers = atoi(argv[1]);
-    int numConsumers = atoi(argv[2]);
+    numProducers = atoi(argv[1]);
+    numConsumers = atoi(argv[2]);
 
     printf("NUMPRODUCERS: %d\n", numProducers);
     printf("NUMCONSUMERS: %d\n", numConsumers);
@@ -87,17 +103,20 @@ int main(int argc, char *argv[]){
 
     // Initialize semaphores
     printf("Initializing semaphores...\n");
-    sem_init(&mutex, 0, 0);
-    sem_init(&empty, 0, 1);
-    sem_init(&full, 0, 0);
+    sem_init(&mutex, 0, 1);
+    sem_init(&empty, 0, 0);
+    sem_init(&full, 0, 1);
     printf("\tDone\n");
 
     // Create producers
     printf("Creating producer threads...\n");
-    pthread_t **producers = malloc(sizeof(pthread_t*) * numProducers);
+    pthread_t *producers = malloc(sizeof(pthread_t) * numProducers);
+    if(producers == NULL){
+        printf("Error allocating memory\n");
+        exit(-1);
+    }
     for(int i = 0; i < numProducers; i++){
-        producers[i] = malloc(sizeof(pthread_t));
-        int ret = pthread_create(producers[i], NULL, producerFn, NULL);
+        int ret = pthread_create(&producers[i], NULL, producerFn, NULL);
         if(ret < 0){
             printf("Error spawning producer thread %d\n", i+1);
             exit(-1);
@@ -108,22 +127,27 @@ int main(int argc, char *argv[]){
     // Create consumers
     printf("Creating consumer threads...\n");
     counter = 0;
-    pthread_t **consumers = malloc(sizeof(pthread_t*) * numConsumers);
+    pthread_t *consumers = malloc(sizeof(pthread_t) * numConsumers);
+    if(consumers == NULL){
+        printf("Error allocating memory\n");
+        exit(-1);
+    }
     for(int i = 0; i < numConsumers; i++){
-        consumers[i] = malloc(sizeof(pthread_t));
-        int ret = pthread_create(consumers[i], NULL, consumerFn, NULL);
+        int ret = pthread_create(&consumers[i], NULL, consumerFn, NULL);
         if(ret < 0){
-            printf("Error spawning producer thread %d\n", i+1);
+            printf("Error spawning consumer thread %d\n", i+1);
             exit(-1);
         }
     }
     printf("\tDone\n");
 
     for(int i = 0; i < numProducers; i++){
-        pthread_join(*producers[i], NULL);
+        pthread_join(producers[i], NULL);
     }
+    printf("ALL PRODUCERS FINISHED\n");
 
     for(int i = 0; i < numConsumers; i++){
-        pthread_join(*consumers[i], NULL);
+        pthread_join(consumers[i], NULL);
     }
+    printf("ALL CONSUMERS FINISHED\n");
 }
--- linked_list.c
+++ linked_list.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <semaphore.h>
 
 #include "linked_list.h"
@@ -11,34 +12,53 @@ struct Node {
 } *head;
 
 void pushJob(struct PrintJob *job){
+    printf("PUSHING\n");
     struct Node *new = (struct Node *)malloc(sizeof(struct Node));
+    if(new == NULL){
+        printf("Error allocating memory\n");
+        exit(-1);
+    }
     new->job = job;
     new->next = NULL;
 
-    struct Node *last = (struct Node *)head;
-    while(last->next != NULL)
-        last = last->next;
-    last->next = new;
+    if(head == NULL)
+        head = new;
+    else{
+        struct Node *last = (struct Node *)head;
+        while(last->next != NULL)
+            last = last->next;
+        last->next = new;
+    }
 
     int c = count();
-    if(c == QUEUE_MAX) // If queue is now full, lock full semaphore
+    if(c >= QUEUE_MAX) // If queue is now full, lock full semaphore
         sem_wait(&full);
     else if(c == 1) // If queue is no longer empty, free the empty semaphore
         sem_post(&empty);
+    printf("PUSHING DONE\n");
 }
 
 struct PrintJob* popJob(){
+    printf("POPPING\n");
+    if(head == NULL){
+        return NULL;
+    }
     struct Node *temp = head;
     head = head->next;
     struct PrintJob *job = temp->job;
     free(temp);
-    
     int c = count();
+    printf("FUCK %d\n", c);
     if(c == QUEUE_MAX-1) // If queue is no longer full, free the full semaphore
         sem_post(&full);
-    else if(c == 0) // If queue is empty, lock the empty semaphore
-        sem_wait(&empty);
+    if(c != 0)
+        sem_post(&empty);
+    // else if(c == 0){ // If queue is empty, lock the empty semaphore
+    //     printf("FUCKkkkkkkkkkk %d\n", job->size);
+    //     sem_wait(&empty);
+    // }
 
+    printf("POPPING DONE\n");
     return job;
 }
 
